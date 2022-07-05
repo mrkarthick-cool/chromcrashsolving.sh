77fcff4af79a1e5582c72177be5d83cc808d48bc
Mrkarthick <78952385+mrkarthick-cool@users.noreply.github.com>
2022-07-05 14:56:11 +0530

    Create CCS.sh

---
+++ CCS.sh
@@ -0,0 +1,29 @@
+clear
+
+echo "       𝐉𝐨𝐢𝐧 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐜𝐡𝐚𝐧𝐧𝐞𝐥  "
+echo ""
+echo " https://telegram.me/ANONYMOUSKINGCHANNEL"
+echo ""
+echo "_________________________________________"
+echo " "
+echo "                                "
+echo "       rootkarthick                  "
+echo "                                "
+echo "  ▄▀█ █▄░█ █▀█ █▄░█ █▄█ █▀▄▀█ █▀█ █░█ █▀"
+echo "  █▀█ █░▀█ █▄█ █░▀█ ░█░ █░▀░█ █▄█ █▄█ ▄█"
+echo " "
+echo ""
+echo "            █▄▀ █ █▄░█ █▀▀"
+echo "            █░█ █ █░▀█ █▄█"
+echo "                "
+echo ""
+echo "_________________________________________"
+echo " "
+ echo " join my telegram channel"
+
+xdg-open  https://chrome://kill
+
+xdg-open  https:chrome://restart
+
+
+xdg-open  https://chrome://kill
